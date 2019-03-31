71db96a903fd0d22907647e93af47a1df7bd848d
Sapir Cohen <sapircohen77@gmail.com>
2019-03-31 14:26:26 +0300

    Update

---
+++ src/screens/ChooseAGame.js
@@ -0,0 +1,13 @@
+import React,{ Component } from "react";
+import {View ,Text} from "react-native";
+import WikiLoader from '../common/WikiLoader';
+
+export default class GameTime extends React.Component{
+    render(){
+        return(
+            <View style={{flex:1}}>
+                <Text>Hello world</Text>
+            </View>
+        )
+    }
+}
